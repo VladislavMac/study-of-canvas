45eeb05f2eac259d2ccb7235687a7352ae0d361a
Vladisalv Mac <mvvminsk@gmail.com>
2020-11-20 21:37:47 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var 
+  canv  = document.querySelector('#canvas'),
+  ctx   = canv.getContext("2d"); // 2d graph
+
+canv.width = window.innerWidth; // На всю ширину страницы  
+canv.height = window.innerHeight; // На всю высоту
+
+// code
